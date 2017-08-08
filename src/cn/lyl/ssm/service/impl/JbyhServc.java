10c0e8fa94e3465fb8d84e6586845c642637cf78
Administrator <Administrator@WCODCDPJBJRZJ5Z>
2017-08-09 00:33:28 +0800

    编写了基本用户的service实现类

---
+++ src/cn/lyl/ssm/service/impl/JbyhServc.java
@@ -0,0 +1,33 @@
+package cn.lyl.ssm.service.impl;
+
+import java.util.List;
+
+import cn.lyl.ssm.po.Jbyh;
+
+public class JbyhServc extends CommonSevc<Jbyh> {
+
+	@Override
+	public void save(Jbyh arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Jbyh find(String arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Jbyh> findAll(String arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Jbyh arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
